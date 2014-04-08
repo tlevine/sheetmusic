09a29b20f901114a06a56fce49f2625448e88039
Thomas Levine <_@thomaslevine.com>
2014-04-08 21:37:09 +0000

    add spells to plugin.xml

--- spellbook/sheetmusic.py
+++ spellbook/sheetmusic.py
@@ -6,5 +6,6 @@ import mingus.core.intervals as intervals
 def main():
     functions = {}
     functions.update({name:getattr(intervals,name) for name in dir(intervals) if re.match(r'^(major|minor).*', name)})
+    return functions
 
 sheetmusic_functions = main()

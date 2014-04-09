d4829588b0637c46fae0538b074347e07a935cd0
Thomas Levine <_@thomaslevine.com>
2014-04-09 14:36:58 +0000

    -

--- spellbook/sheetmusic.py
+++ spellbook/sheetmusic.py
@@ -9,23 +9,33 @@ def pretty(func):
     Given a function of type (int -> int),
     make it into a function of type (scientific -> scientific).
     '''
-    def wrapper(scientific_notation):
+    def wrapper(scientific_notation, *args, **kwargs):
         m = re.match(r'^([^0-9]+)([0-9]+)$', scientific_notation)
         if not m:
             raise ValueError('%s is not in scientific notation' % scientific_notation)
         note_within_octave = m.group(1)
         octave = int(m.group(2))
         input_int = int(containers.Note(note_within_octave, octave))
-        output_int = func(input_int)
+        output_int = func(input_int, *args, **kwargs)
         note = containers.Note(output_int)
         return note.name + str(note.octave)
+    return wrapper
 
-def interval(up, thenote, theinterval):
-    h
+@pretty
+def interval(note, halfsteps):
+    '''
+    :param note: Integer representation of the note, like 57 for A4
+    :param halfsteps: Integer of half steps in the interval, negative
+                      for decreasing intervals
+    '''
+    return note + halfsteps
+
+class Foo:
+    def __add__(self, n):
+        return 8
 
 def main():
-    functions = {}
-    functions.update({name:getattr(intervals,name) for name in dir(intervals) if re.match(r'^(major|minor).*', name)})
+    functions = {'foo': Foo}
     return functions
 
 sheetmusic_functions = main()

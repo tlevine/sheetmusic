3e80f0861f398e259485f1d475326d2754548219
Thomas Levine <_@thomaslevine.com>
2014-04-09 14:47:56 +0000

    -

--- spellbook/sheetmusic.py
+++ spellbook/sheetmusic.py
@@ -4,24 +4,27 @@ import mingus.containers as containers
 import mingus.core.chords as chords
 import mingus.core.intervals as intervals
 
-def parse_scientific(scientific_notation):
-    m = re.match(r'^([^0-9]+)([0-9]+)$', scientific_notation)
+def from_scientific(scientific_note):
+    m = re.match(r'^([^0-9]+)([0-9]+)$', scientific_note)
     if not m:
-        raise ValueError('%s is not in scientific notation' % scientific_notation)
+        raise ValueError('%s is not in scientific notation' % scientific_note)
     note_within_octave = m.group(1)
     octave = int(m.group(2))
     return containers.Note(note_within_octave, octave)
+
+def to_scientific(integral_note):
+    note = containers.Note(integral_note)
+    return note.name + str(note.octave)
 
 def pretty(func):
     '''
     Given a function of type (int -> int),
     make it into a function of type (scientific -> scientific).
     '''
-    def wrapper(scientific_notation, *args, **kwargs):
-        input_int = int(parse_scientific(scientific_notation))
+    def wrapper(scientific_note, *args, **kwargs):
+        input_int = int(from_scientific(scientific_note))
         output_int = func(input_int, *args, **kwargs)
-        note = containers.Note(output_int)
-        return note.name + str(note.octave)
+        return to_scientific(output_int)
     return wrapper
 
 @pretty
@@ -33,15 +36,20 @@ def interval(note, halfsteps):
     '''
     return note + halfsteps
 
-def integer(note):
+def to_integer(scientific_note):
     '''
     :param note: Scientific representation of the note, like A4
     '''
-    return int(parse_scientific(note))
+    return int(from_scientific(scientific_note))
+
+def from_integer(integral_note):
+    return to_scientific(containers.Note(integral_note))
 
 def main():
     functions = {
-        'integer': integer,
+        'to_integer': to_integer,
+        'from_integer': from_integer,
+        'interval': interval,
     }
     return functions
 

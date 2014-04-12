acb409630e9cfcfd4876f2c87ce217fd09903b36
Thomas Levine <_@thomaslevine.com>
2014-04-12 19:57:53 +0000

    notes

--- spellbook/sheetmusic.py
+++ spellbook/sheetmusic.py
@@ -50,7 +50,7 @@ def main():
         'to_integer': to_integer,
         'from_integer': from_integer,
         'interval': interval,
-        'chord': lambda: [[3,4]],
+        'chord': lambda: [[3,4]], # http://code.google.com/p/mingus/wiki/tutorialChords
         'scale': lambda: [[3],[4]], # http://code.google.com/p/mingus/wiki/tutorialScales
     }
     return functions

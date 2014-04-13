3136983d3f72510da89d6ac01899edd72ea6d16c
Thomas Levine <_@thomaslevine.com>
2014-04-13 03:00:04 +0000

    tests

--- test.py
+++ test.py
@@ -5,11 +5,14 @@ import os
 import nose.tools as n
 import lxml.etree
 
-import sheetmusic
+from sheetmusic import sheetmusic_functions as f
 
 def test_xml():
     fn = os.path.join('spellbook','plugin.xml')
     xml = lxml.etree.parse(fn).getroot()
     from_xml = set(xml.xpath('//function/@name'))
-    from_python = set(sheetmusic.sheetmusic_functions.keys())
+    from_python = set(f.keys())
     n.assert_set_equal(from_xml, from_python)
+
+def test_to_integer():
+    n.assert_equal(f['to_integer']('B3'), 34)

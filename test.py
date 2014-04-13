ef5618a40dd7ca10703415c28db21d9cc3e1974e
Thomas Levine <_@thomaslevine.com>
2014-04-13 02:47:23 +0000

    compare xml to python

--- test.py
+++ test.py
@@ -2,10 +2,14 @@ import sys
 sys.path.append('spellbook')
 import os
 
+import nose.tools as n
 import lxml.etree
 
 import sheetmusic
 
 def test_correspondance():
     fn = os.path.join('spellbook','plugin.xml')
-    lxml.etree.parse(fn).getroot()
+    xml = lxml.etree.parse(fn).getroot()
+    from_xml = set(xml.xpath('//function/@name'))
+    from_python = set(sheetmusic.sheetmusic_functions.keys())
+    n.assert_set_equal(from_xml, from_python)

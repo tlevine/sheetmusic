136842aba0ca04f5ca73b67a1bc84a380f8b6b3b
Thomas Levine <_@thomaslevine.com>
2014-04-13 02:45:18 +0000

    test file

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import sys
 sys.path.append('spellbook')
+import os
+
+import lxml.etree
 
 import sheetmusic
+
+def test_correspondance():
+    fn = os.path.join('spellbook','plugin.xml')
+    lxml.etree.parse(fn).getroot()

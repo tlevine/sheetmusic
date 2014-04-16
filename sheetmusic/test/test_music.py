cb8ecbd715f778f06da0484eabbada27bc019a08
Thomas Levine <_@thomaslevine.com>
2014-04-16 01:22:53 +0000

    prgoression test

--- sheetmusic/test/test_music.py
+++ sheetmusic/test/test_music.py
@@ -13,3 +13,9 @@ def test_scale():
     observed = music.chord('augmented_major_seventh', c.Note('C-5'))
     expected = [[c.Note(x)] for x in ['C-5', 'E-5', 'G#-5', 'B-6']]
     n.assert_list_equal(observed, expected)
+
+def test_progression():
+    observed = music.progression([['I','IV','V']], c.Note('C-3'))
+    expected_str = [['C-3', 'E-3', 'G-3'], ['F-3', 'A-4', 'C-4'], ['G-3', 'B-4', 'D-4']]
+    expected = [[c.Note(j) for j in i] for i in expected_str]
+    n.assert_list_equal(observed, expected)

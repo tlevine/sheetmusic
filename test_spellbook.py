3e6d0f3680b7791fcca40c186af5a484313b9243
Thomas Levine <_@thomaslevine.com>
2014-04-16 00:49:42 +0000

    focus the spellbook tests

--- test_spellbook.py
+++ test_spellbook.py
@@ -13,6 +13,3 @@ def test_xml():
     from_xml = set(xml.xpath('//function/@name'))
     from_python = set(f.keys())
     n.assert_set_equal(from_xml, from_python)
-
-def test_to_integer():
-    n.assert_equal(f['to_integer']('B3'), 34)

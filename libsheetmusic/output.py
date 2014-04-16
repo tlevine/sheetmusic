295b4c93e47551a26ce2f6a1eb2eca9585550ef2
Thomas Levine <_@thomaslevine.com>
2014-04-16 03:02:30 +0000

    output

---
+++ libsheetmusic/output.py
@@ -0,0 +1,10 @@
+import mingus.extra.LilyPond as LilyPond
+from mingus.midi import fluidsynth
+fluidsynth.init
+
+def sheetmusic(range_ref, key = 'C', meter = (4, 4)):
+    for column in cells:
+        bar = containers.Bar(key, meter)
+        for cell in column:
+            bar += from_scientific(cell)
+        LilyPond.to_png(bar, '/tmp/bar.png')
--- spellbook/sheetmusic.py
+++ spellbook/sheetmusic.py
@@ -1,45 +1,7 @@
+import functools
+import Gnumeric
 
-import mingus.extra.LilyPond as LilyPond
-from mingus.midi import fluidsynth
-fluidsynth.init
-
-
-def sheetmusic(range_ref, key = 'C', meter = (4, 4)):
-    for column in cells:
-        bar = containers.Bar(key, meter)
-        for cell in column:
-            bar += from_scientific(cell)
-        LilyPond.to_png(bar, '/tmp/bar.png')
-
-try:
-    import Gnumeric
-except ImportError:
-    pass
-else:
-    def range_values(range_ref):
-        workbook_index = 0 # Let's just hope that's always the case
-        workbook = Gnumeric.workbooks()[workbook_index]
-        sheet_index = int(Gnumeric.functions['sheet'](range_ref)) - 1
-        sheet = workbook.sheets()[sheet_index]
-        begin, end = range_ref.get_tuple()
-        rows = map(int, Gnumeric.functions['row'](range_ref)[0])
-        columns = [int(x[0]) for x in Gnumeric.functions['column'](range_ref)]
-        return [[sheet.cell_fetch(int(column-1), int(row-1)).get_entered_text() for row in rows] for column in columns]
-
-def from_integer(range_ref):
-    return str(type(range_ref))
-
-def from_range_ref(range_ref):
-    workbook_index = 0 # Let's just hope that's always the case
-    workbook = Gnumeric.workbooks()[workbook_index]
-    sheet_index = int(Gnumeric.functions['sheet'](range_ref)) - 1
-    sheet = workbook.sheets()[sheet_index]
-    begin, end = range_ref.get_tuple()
-    print(Gnumeric.functions['row'](range_ref))
-    print(Gnumeric.functions['column'](range_ref))
-    rows = map(int, Gnumeric.functions['row'](range_ref)[0])
-    columns = [int(x[0]) for x in Gnumeric.functions['column'](range_ref)]
-    return [[sheet.cell_fetch(int(column-1), int(row-1)).get_entered_text() for row in rows] for column in columns]
+from_range_ref = functools.partial(sheetmusic.spreadsheet.from_range_ref, Gnumeric)
 
 def main():
     functions = {

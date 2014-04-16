5dcd3eb001fc500156b206cff915d1e41489e63b
Thomas Levine <_@thomaslevine.com>
2014-04-16 21:52:11 +0000

    remove junk from the spellbook

--- spellbook/sheetmusic.py
+++ spellbook/sheetmusic.py
@@ -1,18 +1,2 @@
-import functools
-import Gnumeric
-
-from_range_ref = functools.partial(sheetmusic.spreadsheet.from_range_ref, Gnumeric)
-
-def main():
-    functions = {
-     #  'to_integer': to_integer,
-        'from_range_ref': from_range_ref,
-     #  'interval': interval,
-     #  'chord': chord,
-     #  'scale': scale,
-     #  'progression': progression,
-     #  'sheetmusic': sheetmusic,
-    }
-    return functions
-
-sheetmusic_functions = main()
+import libspreadsheet.main
+sheetmusic_functions = libspreadsheet.main.functions()

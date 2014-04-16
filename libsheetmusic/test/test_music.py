c473d9e9570c52ccf919eeda64f52bbf94129300
Thomas Levine <_@thomaslevine.com>
2014-04-16 15:14:30 +0000

    more tests

--- libsheetmusic/test/test_music.py
+++ libsheetmusic/test/test_music.py
@@ -14,11 +14,24 @@ def test_chord():
     expected = [[c.Note(x)] for x in ['C-5', 'E-5', 'G#-5', 'B-6']]
     n.assert_list_equal(observed, expected)
 
+def test_arpeggio():
+    observed = music.chord('augmented_major_seventh', c.Note('C-5'))
+    expected = [[c.Note(x) for x in ['C-5', 'E-5', 'G#-5', 'B-6']]]
+    n.assert_list_equal(observed, expected)
+
 def test_progression():
     observed = music.progression([['I','IV','V']], c.Note('C-3'))
     expected_str = [['C-3', 'E-3', 'G-3'], ['F-3', 'A-4', 'C-4'], ['G-3', 'B-4', 'D-4']]
     expected = [[c.Note(j) for j in i] for i in expected_str]
     n.assert_list_equal(observed, expected)
 
-def test_interval():
+def test_keyed_interval():
     raise NotImplementedError
+
+def test_nonkeyed_interval():
+    raise NotImplementedError
+
+def test_next_note():
+    n.assert_equal(_next_note(c.Note('D-4'), 'E'), c.Note('E-4'))
+    n.assert_equal(_next_note(c.Note('D-4'), 'C'), c.Note('C-5'))
+    n.assert_equal(_next_note(c.Note('D-2'), 'E'), c.Note('E-2'))

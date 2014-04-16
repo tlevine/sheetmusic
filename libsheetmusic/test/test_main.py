27c44879d12151eb34b7f3adbbba9ae690f2c3c1
Thomas Levine <_@thomaslevine.com>
2014-04-16 21:57:36 +0000

    test case

--- libsheetmusic/test/test_main.py
+++ libsheetmusic/test/test_main.py
@@ -1,6 +1,8 @@
 import os
+import collections
 
 import nose.tools as n
+n.assert_dict_equal.__self__.maxDiff = None
 import lxml.etree
 
 import libsheetmusic.main as main
@@ -11,3 +13,13 @@ def test_functions():
     from_xml = set(xml.xpath('//function/@name'))
     from_python = set(main.functions())
     n.assert_set_equal(from_xml, from_python)
+
+def test_cases():
+    '''
+    Function names are case insensitive.
+    With this in mind, the functions dict
+    should contain no duplicates.
+    '''
+    observed = dict(collections.Counter(k.upper() for k in main.functions().keys()))
+    expected = {k.upper():1 for k in main.functions().keys()}
+    n.assert_dict_equal(observed, expected)

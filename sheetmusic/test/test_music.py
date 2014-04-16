2bc58f7a394e8c452892e93aa7cc36aea186a00c
Thomas Levine <_@thomaslevine.com>
2014-04-16 01:17:14 +0000

    another test

--- sheetmusic/test/test_music.py
+++ sheetmusic/test/test_music.py
@@ -8,3 +8,8 @@ def test_scale():
     expected = [map(c.Note, ['G-3', 'A-4', 'B-4', 'C#-4', 'D-4', 'E-4', 'F#-4'])]
     n.assert_list_equal(observed[0], expected[0])
     n.assert_list_equal(observed, expected)
+
+def test_scale():
+    observed = music.chord('augmented_major_seventh', 'C5')
+    expected = [[c.Note(x)] for x in ['C-5', 'E-5', 'G#-5', 'B-6']]
+    n.assert_list_equal(observed, expected)

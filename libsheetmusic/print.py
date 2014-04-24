3fc1a9045da3ca9d1ed726e76a9f9cf10130edbe
Thomas Levine <_@thomaslevine.com>
2014-04-24 22:53:58 +0000

    printing

--- libsheetmusic/print.py
+++ libsheetmusic/print.py
@@ -1,18 +1,42 @@
+import functools
+
 import mingus.extra.LilyPond as LilyPond
 
 def sheetmusic(Gnumeric, range_ref, key = 'C', meter = (4, 4), header = False):
+    partial_bars = functools.partial(bars, key, meter)
     cells = from_range_ref(Gnumeric, range_ref)
-    upper, lower = meter
     for column in cells:
-        if header:
-            track = containers.Track() #column header
-        else:
-            track = containers.Track()
-        bar = containers.Bar(key, meter)
-        for cell in column:
-            if b.is_full():
-                track += bar
-                bar = containers.Bar(key, meter)
-            else:
-                bar += from_scientific(cell)
-        LilyPond.to_png(bar, '/tmp/bar.png')
+        track = build_track(header, partial_bars, column)
+        return LilyPond.to_png(track, '/tmp/track.png')
+
+def build_track(header, partial_bars, column)
+    '''
+    header :: bool
+    column :: [Note]
+    '''
+
+    if header:
+        instrument = column[0]
+        notes = column[1:]
+    else:
+        instrument = None
+        notes = column
+
+    track = containers.Track(instrument)
+    for bar in partial_bars(notes):
+        track += bar
+    return track
+
+def bars(key, meter, notes):
+    '''
+    Group the notes into bars.
+    '''
+    upper, lower = meter
+    left = notes[:4]
+    right = notes[4:]
+
+    bar = containers.Bar(key, meter)
+    bar.place_notes(the_notes, upper)
+
+    yield bar
+    yield _build_bars(key, meter, right)

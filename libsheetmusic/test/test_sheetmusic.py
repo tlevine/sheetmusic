8e5dc19fc9068ce9f303a1a5c7ff96403ae911a4
Thomas Levine <_@thomaslevine.com>
2014-04-24 23:07:53 +0000

    test for meter

--- libsheetmusic/test/test_sheetmusic.py
+++ libsheetmusic/test/test_sheetmusic.py
@@ -3,7 +3,16 @@ import nose.tools as n
 
 import libsheetmusic.sheetmusic as s
 
-def test_bars():
+def test_bars_D_4_4():
+    observed = list(s.bars('D', (4, 4), ['C-4', 'E-4', 'G-4']))
+    expected = [c.Bar('D', (4, 4))]
+    expected[0] + 'C-4'
+    expected[0] + 'E-4'
+    expected[0] + 'G-4'
+    
+    n.assert_list_equal(observed, expected)
+
+def test_bars_C_2_2():
     observed = list(s.bars('C', (2, 2), ['C-4', 'E-4', 'G-4']))
     expected = [c.Bar('C', (2, 2))] * 2
     expected[0] + 'C-4'

3c78988a074b59017489ba9b332200430322db0c
Thomas Levine <_@thomaslevine.com>
2014-05-05 20:22:01 +0000

    -

--- libsheetmusic/sheetmusic.py
+++ libsheetmusic/sheetmusic.py
@@ -1,4 +1,5 @@
 import functools
+from itertools import izip
 
 import mingus.containers as c
 import mingus.extra.LilyPond as LilyPond
@@ -8,12 +9,17 @@ import libsheetmusic.util as u
 
 def sheetmusic(Gnumeric, range_ref, key = 'C', upper = 4, lower = 4, header = False):
     partial_bars = functools.partial(bars, key, (upper, lower))
-    cells = u.from_range_ref(Gnumeric, range_ref)
-    for column in cells:
-        track = build_track(header, partial_bars, column)
-        return LilyPond.to_png(track, '/tmp/track.png')
+    cells = izip(*u.range_apply(u.from_scientific, u.from_range_ref(Gnumeric, range_ref)))
 
-def build_track(header, partial_bars, column):
+    if header:
+        next(cells) # Skip the header
+
+    for row in cells:
+        beat = build_beat(header, partial_bars, column)
+        lp = LilyPond.from_Track(track)
+        return LilyPond.to_png(lp, '/tmp/track')
+
+def build_beat(header, partial_bars, column):
     '''
     header :: bool
     column :: [Note]

47c5a5651fb6437f5b7e53f09bfcf2ea196120d7
Thomas Levine <_@thomaslevine.com>
2014-05-07 14:28:37 +0000

    try getting optional args working

--- libsheetmusic/main.py
+++ libsheetmusic/main.py
@@ -89,8 +89,15 @@ def gnumeric_functions():
     else:
         def progression(progression_range_ref, string_root_note):
             return s.progression(Gnumeric, progression_range_ref, string_root_note)
-        def sheetmusic(range_ref, key = 'C', upper = 4, lower = 4, header = False):
-            return sm.sheetmusic(Gnumeric, range_ref, key, upper, lower, header)
+        def sheetmusic(range_ref, *args):
+            '''@GNM_FUNC_HELP_NAME@SHEETMUSIC:Print the notes to sheet music.
+@GNM_FUNC_HELP_ARG@number1:The key signature ("A","B","C",...)
+@GNM_FUNC_HELP_ARG@number2:The top number in the time signature ("3" for 3/4 time)
+@GNM_FUNC_HELP_ARG@number3:The bottom number in the time signature ("4" for 3/4 time)
+@GNM_FUNC_HELP_EXAMPLES@=SHEETMUSIC(A1:D20,"F","2","4",true)'''
+            keys = ['key', 'upper', 'lower', 'header']
+            kwargs = zip(keys, args)
+            return sm.sheetmusic(Gnumeric, range_ref, **kwargs)
         def to_midi(fn, range_ref_or_cell):
             return sm.to_midi(Gnumeric, fn, range_ref_or_cell)
         def play(range_ref_or_cell):

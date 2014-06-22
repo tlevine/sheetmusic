391aeffaa0e2469e702ccffaac0dca96a62b9d87
Thomas Levine <_@thomaslevine.com>
2014-06-21 21:17:31 -0400

    sheetmusic package

---
+++ sheetmusic/sheetmusic.py
@@ -0,0 +1,10 @@
+from functools import partial
+
+import libsheetmusic.console as c
+
+import Gnumeric
+
+play = partial(c.play, Gnumeric)
+loop = partial(c.loop, Gnumeric)
+midi = partial(c.midi, Gnumeric)
+sheetmusic = partial(c.sheetmusic, Gnumeric)

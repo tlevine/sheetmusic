3b8955dedfea9e4487fa04b15c1607e1252f73ce
Thomas Levine <_@thomaslevine.com>
2014-07-01 07:53:04 -0400

    functions

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='sheetmusic',
       scripts=['bin/sheetmusic-installer'],
       install_requires = ['mingus', 'lxml'],
       tests_require = ['nose'],
-      version='0.0.2',
+      version='0.0.3',
       license='AGPL',
 )
